c05df2547be28e2e711f96241962f64ebf5a97e2
Ilinykh Ivan <ilinykh.i@skbkontur.ru>
2023-08-27 21:08:10 +0500

    basic pagination

--- components/PokemonList.tsx
+++ components/PokemonList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useSWR from 'swr';
 import axios from 'axios';
 import styled from 'styled-components';
@@ -6,18 +6,29 @@ import styled from 'styled-components';
 const fetcher = (url: string) => axios.get(url).then(res => res.data);
 
 const PokemonList: React.FC = () => {
-  const { data, error } = useSWR<PokemonResponse>('https://pokeapi.co/api/v2/pokemon?limit=20', fetcher);
-
-  if (error) return <div>Error loading data.</div>;
-  if (!data) return <div>Loading...</div>;
-
-  return (
-    <div>
-      {data.results.map((pokemon: { name: string }) => (
-        <div key={pokemon.name}>{pokemon.name}</div>
-      ))}
-    </div>
-  );
-};
-
-export default PokemonList;
+    const [currentPage, setCurrentPage] = useState(1);
+    const itemsPerPage = 20;
+    const offset = (currentPage - 1) * itemsPerPage;
+  
+    const { data, error } = useSWR<PokemonResponse>(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${itemsPerPage}`, fetcher);
+  
+    if (error) return <div>Error loading data.</div>;
+    if (!data) return <div>Loading...</div>;
+  
+    const totalPages = Math.ceil(data.count / itemsPerPage);
+  
+    return (
+      <div>
+        {data.results.map((pokemon) => (
+          <div key={pokemon.name}>{pokemon.name}</div>
+        ))}
+        <div>
+          <button disabled={currentPage === 1} onClick={() => setCurrentPage(currentPage - 1)}>Previous</button>
+          <button disabled={currentPage === totalPages} onClick={() => setCurrentPage(currentPage + 1)}>Next</button>
+        </div>
+        <div>Page {currentPage} of {totalPages}</div>
+      </div>
+    );
+  };
+  
+  export default PokemonList;
